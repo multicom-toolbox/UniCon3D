bedd8dbe67719c5e74fd49d2214f5378381b3f14
jh7x3 <jh7x3@sunflower.rnet.missouri.edu>
2016-07-25 14:13:42 -0500

    Modify scripts/rr2cm.py to support multiple-format residue-residue contact as input to generate contact map

--- scripts/rr2cm.py
+++ scripts/rr2cm.py
@@ -16,10 +16,14 @@ parser.add_option('--fasta', dest='fasta',
 	help = 'fasta file containing the target sequence')
 parser.add_option('--rr', dest='rr',
 	default = '',    # default empty!
-	help = 'rr file in CASP format')
+	help = 'residue-residue file, default in CASP format')
 parser.add_option('--xl', dest='xl',
 	default = 1000000.0,    # default 100000000.0
 	help = 'fraction of top contacts to select as a function of sequence length')
+parser.add_option('--format', dest='format',
+    default = 5,   
+    help = '(option) The rr file format with different column, 2-column(id id), 3-column(id id weight), 5-column(default, CASP format)')
+
 
 
 (options,args) = parser.parse_args()
@@ -27,6 +31,7 @@ parser.add_option('--xl', dest='xl',
 fasta = options.fasta
 rr = options.rr
 xl = float(options.xl)
+format = int(options.format)
 
 #open the fasta file
 f = open(fasta, 'r')
@@ -46,16 +51,35 @@ num = 0
 #open the rr file
 r = open(rr, 'r')
 for line in r:
-	temp = re.split(" +",line)
-	if len(temp) == 5:
-		num += 1
-		if num > maxnum:
-			continue
-		i = int(temp[0].strip()) - 1
-		j = int(temp[1].strip()) - 1
-		p = float(temp[4].strip())
-		CM[i][j] = p
-		CM[j][i] = p
+    #temp = re.split(" +",line)
+    temp = line.split( )
+    if len(temp) == format and format == 5:
+        num += 1
+        if num > maxnum:
+            continue
+        i = int(temp[0].strip()) - 1
+        j = int(temp[1].strip()) - 1
+        p = float(temp[4].strip())
+        CM[i][j] = p
+        CM[j][i] = p
+    elif len(temp) == format and format == 2:
+        num += 1
+        if num > maxnum:
+            continue
+        i = int(temp[0].strip()) - 1
+        j = int(temp[1].strip()) - 1
+        p = 1 # assume all confidence is 1
+        CM[i][j] = p
+        CM[j][i] = p
+    elif len(temp) == format and format == 3:
+        num += 1
+        if num > maxnum:
+            continue
+        i = int(temp[0].strip()) - 1
+        j = int(temp[1].strip()) - 1
+        p = float(temp[2].strip())
+        CM[i][j] = p
+        CM[j][i] = p
 r.close()    # close it		
 
 #now get ready to print it
